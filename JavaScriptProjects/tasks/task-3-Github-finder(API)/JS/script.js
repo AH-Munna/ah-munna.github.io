88f1b5847c7738f5fd16e3a190447b76b9d67ce2
Ahsanul Haqeu Munna <96417158+AH-Munna@users.noreply.github.com>
2022-01-17 17:08:15 +0600

    finalizing task-3

--- JavaScriptProjects/tasks/task-3-Github-finder(API)/JS/script.js
+++ JavaScriptProjects/tasks/task-3-Github-finder(API)/JS/script.js
@@ -1,12 +1,26 @@
 document.getElementById('btnSearch').addEventListener("click", e => {
-    document.getElementById('profile').innerHTML = "";
-
     let userName = document.getElementById('txtUser').value;
-    fetch(`https://api.github.com/users/${userName}`)
-    .then(data => data.json())
-    .then(data => {
-        console.log(data);
-        UI.showPFP(data.avatar_url);
-    })
-    document.getElementById('profile').style.visibility = "visible";
+    if (userName === '') {
+        document.getElementById('profile').style.visibility = "hidden";
+    } else {
+        document.getElementById("profileImage").innerHTML = "";
+        document.getElementById('repos').innerHTML = "";
+
+        fetch(`https://api.github.com/users/${userName}`)
+        .then(data => data.json())
+        .then(data => {
+            console.log(data);
+            UI.showPFP(data.avatar_url);
+            UI.showBtnProfile(data.html_url);
+
+            UI.showRepos(data.public_repos, "Public Repos");
+            UI.showRepos(data.public_gists, "Public Gists");
+            UI.showRepos(data.followers, "Followers");
+            UI.showRepos(data.following, "Following");
+
+            UI.showInfo('Company', data.company);
+            UI.showInfo('Twitter', data.twitter_username);
+        })
+        document.getElementById('profile').style.visibility = "visible";
+    }
 });
--- JavaScriptProjects/tasks/task-3-Github-finder(API)/JS/ui.js
+++ JavaScriptProjects/tasks/task-3-Github-finder(API)/JS/ui.js
@@ -3,9 +3,40 @@ class UI {
         let imgContainer = document.createElement('img');
         imgContainer.setAttribute = 'src alt';
         imgContainer.src = img;
-        imgContainer.className = 'p-2 m-2'
+        imgContainer.className = 'p-2 mx-2 mt-2 img-fluid';
         imgContainer.alt = "profile image";
+
         console.log(imgContainer);
-        document.getElementById('profile').appendChild(imgContainer);
+        document.getElementById('profileImage').appendChild(imgContainer);
+    }
+
+    static showBtnProfile(url) {
+        let btnProfile = document.createElement('a');
+        btnProfile.setAttribute = 'href'
+        btnProfile.href = url;
+        btnProfile.textContent = 'View Profile';
+        btnProfile.className = 'btn btn-outline-primary btn-block p-1 m-2';
+        document.getElementById('profileImage').appendChild(btnProfile);
+    }
+
+    static showRepos(num, label) {
+        let reposContainer = document.createElement('div');
+        reposContainer.className = 'text-center mx-2 my-2 bg-info text-white py-1 px-4 rounded-pill inline-block';
+
+        let reposPub = document.createElement('div');
+        reposPub.textContent = `${label}: ${num}`;
+        reposContainer.appendChild(reposPub);
+
+        document.getElementById('repos').appendChild(reposContainer);
+    }
+
+    static showInfo(infoLabel, info) {
+        let uList = document.createElement('ul');
+        let infoItems = document.createElement('li');
+        infoItems.textContent = `${infoLabel}: ${info}`;
+        
+        uList.appendChild(infoItems);
+        document.getElementById('info').appendChild(uList);
+        console.log(uList.parentElement);
     }
 }
