b8ba8a81c6fbd344b58a69f4e74d96d872a2ad78
Ahsanul Haqeu Munna <96417158+AH-Munna@users.noreply.github.com>
2022-01-17 15:20:16 +0600

    Update ui.js

--- JavaScriptProjects/tasks/task-3-Github-finder(API)/JS/ui.js
+++ JavaScriptProjects/tasks/task-3-Github-finder(API)/JS/ui.js
@@ -2,7 +2,6 @@ class UI {
     static showPFP(img) {
         let imgContainer = document.createElement('img');
         imgContainer.setAttribute = 'src alt';
-        img = 'https://i.imgur.com/LskHhCZ.png';
         imgContainer.src = img;
         imgContainer.className = 'p-2 m-2'
         imgContainer.alt = "profile image";
